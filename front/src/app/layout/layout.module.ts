6ceb07d4770c2f9568b617708a6adff09106c131
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-22 14:41:31 +0200

    components

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, BodyComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ],
-  exports: [LayoutComponent]
+  exports: [LayoutComponent, BodyComponent, HeaderComponent, FooterComponent]
 })
 export class LayoutModule { }
