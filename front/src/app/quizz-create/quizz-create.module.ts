66b141db686f240e1cd9e9c8fd99d5ce22148ef4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-23 14:25:43 +0200

    css ok create quizz

--- front/src/app/quizz-create/quizz-create.module.ts
+++ front/src/app/quizz-create/quizz-create.module.ts
@@ -6,12 +6,14 @@ import { CreateComponent } from './create/create.component';
 import { SetupComponent } from './setup/setup.component';
 import { AddQuestionComponent } from './add-question/add-question.component';
 import { FinishedComponent } from './finished/finished.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
   declarations: [CreateComponent, SetupComponent, AddQuestionComponent, FinishedComponent],
   imports: [
     CommonModule,
+    FontAwesomeModule,
     QuizzCreateRoutingModule
   ]
 })
