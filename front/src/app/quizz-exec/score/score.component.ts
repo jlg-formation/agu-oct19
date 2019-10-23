48204c469b86daf5ae20e0a4b5a52d008a269b94
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-23 15:34:34 +0200

    ok

--- front/src/app/quizz-exec/score/score.component.ts
+++ front/src/app/quizz-exec/score/score.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSmile } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-score',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoreComponent implements OnInit {
 
+  faSmile = faSmile;
   constructor() { }
 
   ngOnInit() {
