255c155c115de5beeaf5a914e780b374f16c9278
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-24 11:16:06 +0200

    form reactif

--- front/src/app/quizz-create/add-question/add-question.component.ts
+++ front/src/app/quizz-create/add-question/add-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-question',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-question.component.scss']
 })
 export class AddQuestionComponent implements OnInit {
+
+  f = new FormGroup({
+    label: new FormControl('Quelle est la capitale de la Belgique ?', Validators.required),
+    answerA: new FormControl('Paris', Validators.required),
+    answerB: new FormControl('Bruxelles', Validators.required),
+    answerC: new FormControl('Rome', Validators.required),
+    answerD: new FormControl('Bucarest', Validators.required),
+    correctAnswer: new FormControl('', Validators.required),
+  });
 
   constructor() { }
 
