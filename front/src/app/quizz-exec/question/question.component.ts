9f9d620777601a4b48f263b647b37f2b56fa4c3a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-24 14:50:38 +0200

    observable params

--- front/src/app/quizz-exec/question/question.component.ts
+++ front/src/app/quizz-exec/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-question',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
-
-  constructor() { }
+  id: number;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      console.log('params: ', params);
+      this.id = +params.id;
+    });
   }
 
 }
