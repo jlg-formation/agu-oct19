cc3213fccea39c10e66c6ba13e1a11271c85d644
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-24 11:53:02 +0200

    add router

--- front/src/app/quizz-create/add-question/add-question.component.ts
+++ front/src/app/quizz-create/add-question/add-question.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-question',
@@ -17,13 +18,14 @@ export class AddQuestionComponent implements OnInit {
     correctAnswer: new FormControl('', Validators.required),
   });
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   submit() {
     console.log('submit coucou');
+    this.router.navigateByUrl('/setup');
   }
 
 }
