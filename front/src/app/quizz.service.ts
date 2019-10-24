ea7d68e990ca8002741c8169138c0ec8bb9b959f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-24 12:38:24 +0200

    finished

--- front/src/app/quizz.service.ts
+++ front/src/app/quizz.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Quizz } from './quizz';
 import { Question } from './question';
+import { Store } from './store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizzService {
 
+
   current = this.getCurrent();
+  store = this.getStore();
 
   constructor() {
   }
@@ -31,8 +34,30 @@ export class QuizzService {
     return q;
   }
 
+
+  saveStore() {
+    localStorage.setItem('store', JSON.stringify(this.store));
+  }
+
+  getStore(): Store {
+    const str = localStorage.getItem('store');
+    if (!str) {
+      return {};
+    }
+    const store = JSON.parse(str);
+    for (const quizz of Object.values(store)) {
+      Object.setPrototypeOf(quizz, Quizz.prototype);
+    }
+    return store;
+  }
+
   addQuestion(question: Question) {
     this.current.questions.push(question);
     this.saveCurrent();
+  }
+
+  addQuizz() {
+    this.store[this.current.name] = this.current;
+    this.saveStore();
   }
 }
