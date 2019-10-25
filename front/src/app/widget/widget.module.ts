4fdfb877b3c4ab29a81c35a8e3a97021f309e4cc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-25 14:55:53 +0200

    button radio ok

--- front/src/app/widget/widget.module.ts
+++ front/src/app/widget/widget.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AutofocusDirective } from './autofocus.directive';
 import { ShowMaxLengthComponent } from './show-max-length/show-max-length.component';
+import { RadioButtonComponent } from './radio-button/radio-button.component';
 
 
 
 @NgModule({
-  declarations: [AutofocusDirective, ShowMaxLengthComponent],
+  declarations: [AutofocusDirective, ShowMaxLengthComponent, RadioButtonComponent],
   imports: [
     CommonModule
   ],
-  exports: [AutofocusDirective, ShowMaxLengthComponent]
+  exports: [AutofocusDirective, ShowMaxLengthComponent, RadioButtonComponent]
 })
 export class WidgetModule { }
