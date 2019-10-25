b35a1e756b835679b35718eb86b8f106d389a727
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-25 09:34:06 +0200

    cors

--- back/server.js
+++ back/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const serveIndex = require('serve-index');
+const cors = require('cors');
 
 const app = express();
 const port = 8000;
+
+app.use(cors());
+
+app.get('/ws/quizz', (req, res, next) => {
+    res.json({ toto: 123 });
+});
 
 app.use(express.static('.'));
 app.use(serveIndex('.', { icons: true }));
